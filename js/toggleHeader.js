608592ffb74329de337a15954556a3e48e283cbd
Alex <alex.kliger@gmail.com>
2022-05-21 20:56:45 -0400

    Added opaque overlay when header visible and enabled toggle by clicking on header or overlay.

--- js/toggleHeader.js
+++ js/toggleHeader.js
@@ -1,25 +1,30 @@
 function toggleHeader() {
-    header.classList.contains('is-hidden')
+    document.body.classList.contains('header-hidden')
     ?
-    header.classList.remove('is-hidden')
+    document.body.classList.remove('header-hidden')
     :
-    header.classList.add('is-hidden')
+    document.body.classList.add('header-hidden')
 }
 
 function matchMediaQuery(query) {
     mediaQuery.match
     ?
-    header.classList.remove('is-hidden')
+    document.body.classList.remove('header-hidden')
     :
-    header.classList.add('is-hidden')
+    document.body.classList.add('header-hidden')
 }
 
-const toggle = document.querySelector('.header-toggle')
 const header = document.querySelector('header')
+const toggleButton = document.querySelector('.header-toggle')
+const opaqueOverlay = document.querySelector('.opaque-overlay')
 const mediaQuery = window.matchMedia('(max-width: 1600px')
-// Add is-hidden class to header at run time if media query matches.
+// Add header-hidden class to header at run time if media query matches.
 matchMediaQuery(mediaQuery)
 // Attach listener for state changes.
-mediaQuery.addEventListener("change", matchMediaQuery)
+mediaQuery.addEventListener('change', matchMediaQuery)
 // Attach listener for toggle button.
-toggle.addEventListener('click', toggleHeader)
+toggleButton.addEventListener('click', toggleHeader)
+// Attach listener to opaque overlay so that anywhere toggles the header.
+opaqueOverlay.addEventListener('click', toggleHeader)
+// Attach listener so that clicking anywhere on header toggles the header.
+header.addEventListener('click', toggleHeader)
